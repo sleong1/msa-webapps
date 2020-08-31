b0eec95dc19ceda71ba843a092f55a89f47c00a5
Su <leongsuwen@yahoo.com>
2020-08-31 19:17:02 +1000

    Rearranging files

--- app.py
+++ app.py
@@ -1,33 +1,4 @@
-import os
-import random
-import getpass
-from flask import Flask, request, render_template, flash
-
-app = Flask(__name__)
-app.secret_key = os.urandom(1)
-
-user = getpass.getuser().title()
-
-# Define a route for the app's home page
-@app.route("/")
-def index():
-    rand_idx = [random.randint(0, AllRecipes.query(Name).count()) for i in range(5)]
-    suggested = [AllRecipes.query.all()[i] for i in rand_idx]
-    return render_template("index.html", username=user, suggested=suggested)
-
-# Define a route for the app's page of all recipes
-@app.route("/recipes")
-def recipes():
-    return render_template("recipes.html")
-
-
-# Define a route for the app's About page
-@app.route("/favourites")
-def favourites():
-    return render_template("favourites.html")
-
-
-
+from app import app
 
 ## Code for logging in
 # @app.route('/login',  methods=["GET", "POST"])
--- apps/routes.py
+++ apps/routes.py
@@ -1,10 +1,28 @@
 from flask import render_template
 from app import app, models
+import random
+import getpass
+
+app.secret_key = os.urandom(1)
 
 AllRecipes = models.AllRecipes
+user = getpass.getuser().title()
 
-@app.route('/')
+# Define a route for the app's home page
+@app.route("/")
 def index():
-    title = 'My Recipe Book'
-    recipes = AllRecipes.query.all()
-    return render_template("index.html", title=title, recipes=recipes)
+    rand_idx = [random.randint(0, AllRecipes.query(Name).count()) for i in range(5)]
+    suggested = [AllRecipes.query.all()[i] for i in rand_idx]
+    return render_template("index.html", username=user, suggested=suggested)
+
+# Define a route for the app's page of all recipes
+@app.route("/recipes")
+def recipes():
+    return render_template("recipes.html")
+
+
+# Define a route for the app's About page
+@app.route("/favourites")
+def favourites():
+    return render_template("favourites.html")
+
