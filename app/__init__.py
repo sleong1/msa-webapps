cd35632d972d23977c60ed5da6b7ccd04dc7ae0c
Su <leongsuwen@yahoo.com>
2020-09-04 11:53:50 +1000

    Hosting

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,13 @@
 import os
+import urllib.parse
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+db_string = os.environ['dbstring']
+params = urllib.parse.quote_plus(db_string)
+
 app.secret_key = os.urandom(1)
 
 # SQL set up
@@ -10,7 +15,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 app.config['SQLALCHEMY_DATABASE_URI'] =\
 	'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 
 db = SQLAlchemy(app)
 
